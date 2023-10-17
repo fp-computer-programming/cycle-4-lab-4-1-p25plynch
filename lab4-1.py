b86f7e5167000ab14c99591f51fc2c71e97bb066
p25plynch <144147950+p25plynch@users.noreply.github.com>
2023-10-17 09:07:03 -0400

    Add files via upload

---
+++ lab4-1.py
@@ -0,0 +1,10 @@
+# Author: PJ Lynch
+# Prompt to enter my first name and store it with a variable.
+first_name = input("Please enter your first name: ")
+# Prompt to enter my last name and store it with an appropriate variable.
+last_name = input("Please enter your last name: ")
+# Create a new variable for my full name and set it equal to the first name variable plus the last name variable.
+full_name = first_name + " " + last_name
+# Print full name.
+print("User's full name is:", full_name)
+
